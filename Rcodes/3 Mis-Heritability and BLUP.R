5673d7ec0633afe288c6aab656a10945cdaa587b
Yongli Zhao <46494340+yzhao4651@users.noreply.github.com>
2024-03-24 11:59:38 -0400

    Update 3 Mis-Heritability and BLUP.R

--- Rcodes/3 Mis-Heritability and BLUP.R
+++ Rcodes/3 Mis-Heritability and BLUP.R
@@ -1,24 +1,26 @@
-###import the normalited data，traits1718normalited5 includes all of 38 traits for this study only 8 traits were used for this article
-###import the normalited data,traits1718normalited5 includes all of 38 traits for this study only 8 traits were used for this article
-normadata <- read.csv("data/traits1718normalited5.csv",na.strings = c("",".","NA"),row.names=1)
+###import the normalited data，traits1718normalited_Yld includes all of 38 traits for this study only 8 traits were used for this article
+###import the normalited data,traits1718normalited_Yld includes all of 38 traits for this study only 8 traits were used for this article
+normadata <- read.csv("data/traits1718normalited_Yld.csv",na.strings = c("",".","NA"),row.names=1)
 ###check the data format
 str(normadata)
 ###change the format of the several variables 
-normadata$GS <- as.numeric(as.character(normadata$GS))
-normadata$SRD <- as.numeric(as.character(normadata$SRD))
 normadata$Entry=as.factor(normadata$Entry)
 normadata$Rep=as.factor(normadata$Rep)
 normadata$Year=as.factor(normadata$Year)
 ###check the data format
 str(normadata)
-###get the heritablity of all the traits using the function in the Function
-source("Function/Heritability.OWA.R")
-Herit <- Heritability(5,38,normadata)
-write.csv(Herit, file = "data/heritabilityall5.csv", na = ".") ### this one is for all of the traits for this project. 
+
+colnames(normadata)[which(names(normadata)=="CmDW")] <- "CmDW_g"
+colnames(normadata)[which(names(normadata)=="Cml")] <- "Cml_cm"
+colnames(normadata)[which(names(normadata)=="CmD_BI")] <- "CmD_BI_mm"
+colnames(normadata)[which(names(normadata)=="CmD_LI")] <- "CmD_LI_mm"
+colnames(normadata)[which(names(normadata)=="CmN")] <- "CmN."
+colnames(normadata)[colnames(normadata)=="Bcirc_cm"] <- "Bcirc"
+colnames(normadata)[colnames(normadata)=="CCirc_cm"] <- "CCirc"
 
 ###get the heritablity of all the traits using the function in the Function
 source("Function/BLUP.R")
-ranefvalueall <- ranefvalue(5,38,normadata)
+ranefvalueall <- ranefvalue(5,17,normadata)
 #write.csv(ranefvalueall,file="data/ranefvalueallcheck.csv")
 
-write.csv(ranefvalueall, file = "data/ranefvalueall.csv", row.names = T, na = ".")
+write.csv(ranefvalueall, file = "data/ranefvalueChapter2.csv", row.names = T, na = ".")
