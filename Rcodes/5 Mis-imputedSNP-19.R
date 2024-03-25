a68d79c8df0592183556c679a1ba99c4e1c9cd64
Yongli Zhao <46494340+yzhao4651@users.noreply.github.com>
2024-03-25 12:29:50 -0400

    Update 5 Mis-imputedSNP-19.R

--- Rcodes/5 Mis-imputedSNP-19.R
+++ Rcodes/5 Mis-imputedSNP-19.R
@@ -1,13 +1,16 @@
-############step 1 emerge all the phenotype data with the index data
-############step 1 emerge all the phenotype data 
 
+####this one is to obtain the imputed SNP, polulation instrcute for GAPIT and rrBLUP software
+####this one is to obtain the imputed SNP, polulation instrcute for GAPIT and rrBLUP software
+
+############step 1 emerge all the phenotype data with the index data
+############step 1 emerge all the phenotype data with the index data
 ### data 1 import the index data with number matched Entry of phenotype and name of SNP 
 Altable <- read.csv("data/Altable19.csv", header = TRUE, na="")
 ###check the data format
 str(Altable)
 
 ###data2 import the  all traits 
-alltraitsblup <- read.csv("data/alltraitsblup.csv", stringsAsFactors = FALSE, header = TRUE,row.names = 1)
+alltraitsblup <- read.csv("data/ranefvalueChapter2.csv", stringsAsFactors = FALSE, header = TRUE,row.names = 1)
 
 ###check the data format 
 str(alltraitsblup)
@@ -19,40 +22,24 @@ str(allblup)
 colnames(allblup)[colnames(allblup)=="Accession"] <- "Taxa"
 ###remove the colomn of Entry
 allblup <- allblup[,-2]
-allblup <- allblup[rowSums(is.na(allblup)) != 22, ]
+allblup[allblup == "."] <- NA
+allblup <- allblup[!(rowSums(is.na(allblup)) == 13), ]
+
 ###check the data format 
 str(allblup)
 ###write out the data set 
-write.csv(allblup, file = "data/alltraits.csv",row.names = T)
+write.csv(allblup, file = "data/alltraits_chapter2.csv",row.names = T)
 
-### this one for OWA
-alltraitsblup <- read.csv("OWA/OWA1819ranef.csv", na.strings = c("",".","NA"), header = TRUE,row.names = 1)
-str(alltraitsblup)
-colnames(alltraitsblup)[colnames(alltraitsblup)=="X"] <- "Entry"
-colnames(alltraitsblup)[colnames(alltraitsblup)=="OWA.x"] <- "OWA18"
-colnames(alltraitsblup)[colnames(alltraitsblup)=="OWA.y"] <- "OWA19"
-str(alltraitsblup)
-####combine the traits data with indexdata  
-allblup <- plyr::join_all(list(Altable[1:2],alltraitsblup), by='Entry')
-str(allblup)
-##remove some Entry is 7,11,129,148,149,150
-allblup <- allblup[!(allblup$Entry==7|allblup$Entry==11|allblup$Entry==129|allblup$Entry==148|allblup$Entry==149|allblup$Entry==150),]
-###check the data format and delete the missing value
-allblup <- na.omit(allblup)
-str(allblup)
-###rename of the column name of Acession to Taxa 
-colnames(allblup)[colnames(allblup)=="Accession"] <- "Taxa"
-allblup <- allblup[,c(1,3:4)]
-###write out the dataset 
-write.csv(allblup, file = "data/alltraitsOWA2.csv")
+
 ############# step2 Download the genotype data
 ############# step2 Download the genotype data
 ############download the SNP data that Lindsay gave to me ################### 
 #### import the SNP values with already imputed missing value datacomb2
 ###trying to load in to GitHub, But this one is big, can not load from here. 
-load("~/Documents/MiacnathusSNPinformation /160324EMimputedSNP_Msi.RData")
+load("data/160324EMimputedSNP_Msi.RData")
+#load("~/Documents/MiacnathusSNPinformation /160324EMimputedSNP_Msi.RData")
 # load("~/DOE Msi study/yield manuscript/phenotypic and GWAS analysis/160324EMimputedSNP_Msi.RData") # Lindsay's version
-load("C:/Users/Admin/Desktop/New folder/miscanthus study-1/Misthcanthus CCA data analysis/160324EMimputedSNP_Msi.RData")
+#load("C:/Users/Admin/Desktop/New folder/miscanthus study-1/Misthcanthus CCA data analysis/160324EMimputedSNP_Msi.RData")
 ### this data set in value part, change the value into data.frame
 ##check the name of that value:
 names(myA.EM.Msi)
@@ -117,8 +104,7 @@ mymatg <- cbind(Taxa,mymatg)
 myGDorderg <-mymatg[order(mymatg$Taxa),]
 #### write out the phenotype with correct TAXA for GAPIT analysis 
 write.csv(myGDorderg, file = "data/myGDimputedSNP19.csv",row.names = FALSE)
-setwd("C:/Users/Admin/Desktop/Miscanthus/Miscanthus")
-write.csv(myGDorderg, file = "data/myGDimputedSNP19OWA2.csv",row.names = FALSE)
+
 # free up RAM by removing large unused objects
 rm(myA.EM.Msi, mymat, mymatg, SNP)
 
@@ -130,8 +116,8 @@ myYorderg <- droplevels(myYorderg)
 #myYorderg <- na.omit(myYorderg[order(myYorderg$Taxa),])
 str(myYorderg)
 #### write out the phenotype with correct TAXA
-write.csv(myYorderg, file = "data/myYimputedSNP19.csv", row.names = FALSE, na = "")
-write.csv(myYorderg, file = "data/myYimputedSNP19OWA2.csv", row.names = FALSE, na = "")
+write.csv(myYorderg, file = "data/myYimputedSNP19_chapter2.csv", row.names = FALSE, na = "")
+
 
 ##############step7 myQ: population structure
 ##############step7 myQ: population structure
@@ -150,18 +136,16 @@ myQorder <- myQ1[order(myQ1$Taxa),]
 myQorder <- myQorder[,c(10,2:9)]
 str(myQorder)
 ####write out the dataset
-write.csv(myQorder, file = "data/myQimputedSNP19.csv",row.names = FALSE)
-write.csv(myQorder, file = "data/myQimputedSNP19OWA2.csv",row.names = FALSE)
 
 #############step8 myGM:Genetic mapping information
 #############step8 myGM:Genetic mapping information
 ########## GM (Genetic mapping dataset)###############################
 #load GM dataset get myGM and myQ
-load("~/Documents/MiacnathusSNPinformation /161025forGAPIT.RData")
+load("161025forGAPIT.RData")
 #load("~/DOE Msi study/yield manuscript/phenotypic and GWAS analysis/161025forGAPIT.RData") # on Lindsay's computer
 ### select the GM dataset with the same ID with GD
 #myGDorderg <- read.csv("~/Documents/whole traits/myGDordfrrblup19.csv")
-load("C:/Users/Admin/Desktop/New folder/miscanthus study-1/Misthcanthus CCA data analysis/161025forGAPIT.RData")
+#load("C:/Users/Admin/Desktop/New folder/miscanthus study-1/Misthcanthus CCA data analysis/161025forGAPIT.RData")
 n <- data.frame(myGDorderg,row.names = 1)
 a <- data.frame(t(n))
 myGM2 <- myGM[match(dimnames(a)[[1]],myGM$Name, nomatch=0),]
@@ -172,8 +156,8 @@ myGM2 <- myGM2[snporder,]
 myGDorderg <- myGDorderg[,c(1, snporder + 1)]
 identical(as.character(myGM2$Name), colnames(myGDorderg)[-1])
 ####output GM data with csv format
-write.csv(myGM2, "data/myGMimputedSNP19.csv",row.names = FALSE)
-write.csv(myGM2, "data/myGMimputedSNP19OWA2.csv",row.names = FALSE)
+write.csv(myGM2, "data/myGMimputedSNP19_chapter2.csv",row.names = FALSE)
+
 #### this one for rrBLUP
 #### this one for rrBLUP
 ####rrBLUP analysis
@@ -181,7 +165,7 @@ write.csv(myGM2, "data/myGMimputedSNP19OWA2.csv",row.names = FALSE)
 myGDorder <-data.frame(cbind(myGDorderg[1],myGDorderg[-1]+1))
 str(myGDorder)
 #### write out the phenotype with correct TAXA
-write.csv(myGDorder, file = "data/myGDimputedSNPrrblup19.csv",row.names = FALSE)
-# note from Lindsay -- I haven't edited this final section because I am not 
-# totally sure what you are trying to accomplish.
+write.csv(myGDorder, file = "data/myGDimputedSNPrrblup19_chapter2.csv",row.names = FALSE)
+
+
 
